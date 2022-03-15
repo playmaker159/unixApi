fdc8a831b82ffc4aeb6c8472a2a0b2c09793c9d3
G G ASHWIN PRABHU <65995972+playmaker159@users.noreply.github.com>
2022-03-15 10:34:57 +0530

    Add files via upload

---
+++ sys.c
@@ -0,0 +1,20 @@
+#include "sys.h" 
+#include <sys/wait.h>
+#include <sys/types.h>
+int main(void)
+{
+int	 status;
+if ((status = system("date")) < 0) 
+	err_sys("system() error");
+pr_exit(status);
+ 
+if ((status = system("nosuchcommand")) < 0) 
+	err_sys("system() error");
+pr_exit(status);
+ 
+if ((status = system("who; exit 44")) < 0) 
+	err_sys("system() error");
+pr_exit(status);
+ 
+exit(0);
+}
